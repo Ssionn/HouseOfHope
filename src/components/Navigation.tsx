546328488dda132d23855f155e3b5dd63169d659
Ssionn <casper.kizewski.developer@gmail.com>
2024-09-20 20:01:04 +0200

    Added a brief logo and button to go to the dashboard

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const NavigationLink = ({ href, children }) => {
+  return (
+    <li>
+      <Link href={href} className="text-lg underline font-semibold">
+        {children}
+      </Link>
+    </li>
+  );
+};
+
+export default function Navigation() {
+  return (
+    <div className="px-4 sm:px-12 md:px-24 lg:px-36">
+      <div className="flex justify-between items-center h-16 w-full mt-20 px-4 sm:px-12 md:px-16 lg:px-24">
+        <div>
+          <Image
+            src="/images/logo.svg"
+            width={64}
+            height={64}
+            alt="House of Hope"
+          />
+        </div>
+        <div>
+          <ul>
+            <NavigationLink href="/dashboard">Dashboard</NavigationLink>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
